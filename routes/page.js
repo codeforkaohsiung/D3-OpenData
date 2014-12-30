e1033e798b4ea567386bb1729493613d4607259d
liao <gonsakon@gmail.com>
2014-12-31 00:35:31 +0800

    整理資料庫結構

--- routes/page.js
+++ routes/page.js
@@ -11,6 +11,18 @@ exports.showcase = function(req, res) {
     res.render('pages/showcase');
 };
 exports.demopage = function(req, res) {
-    res.render('pages/demopage');
-};
+    res.render('pages/demopage', {
+       chartType_name : '圓餅圖',
+       chartType_value : 'pie',
+       xAxis_name : 'gsx$時間',
+       xAxis_value : 'gsx$時間',
+       data_name : "",
+       data_value : "['gsx$新北市總計', 'gsx$臺北市總計', 'gsx$高雄市總計']",
+       range_name : 'gsx$時間',
+       range_min : 5,
+       range_max : 13,
+       range_minimum : 'true',
+       range_maximum : 'true',
 
+    });
+};
